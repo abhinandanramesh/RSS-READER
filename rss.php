54a852a53cc12fd6ea54fdc97b34d68088fd0b1b
Abhinandan Kelgere Ramesh <abhinandanramesh@gmail.com>
2014-07-12 17:45:41 +0530

    Create rss.php

---
+++ rss.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+
+<html>
+  <head>
+    <title>My RSS Reader</title>
+  </head>
+  <body>
+    <h1>Newyoek Times</h1>
+    <ul>
+      <?
+    
+        $dom = simplexml_load_file("http://feeds.nytimes.com/nyt/rss/Technology");
+        foreach ($dom->channel->item as $item)
+      
+        {
+          print "<li>";
+          print "<a href='$item->link}'>";
+          print $item->title;
+          print "</a>";
+          print "</li>";
+        }
+      
+      ?>
+    </ul>
+  </body>
+</html>
